366a9c61e388bef1449fa3e723f3389d6eb6d4e5
MauricioDa <mutrilla@customsoft.biz>
2022-04-12 11:02:47 -0500

    uso de async para peticiones http, subscripcion y desubscripcion

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -8,15 +8,15 @@ import { DataService } from '../../services/data.service';
 })
 export class PostsComponent implements OnInit {
 
-  mensajes: any[] = [];
+  mensajes: any;
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
-    this.dataService.getPosts()
-      .subscribe((posts: any) => {
-        console.log(posts);
-        this.mensajes = posts;
-      });
+    this.mensajes = this.dataService.getPosts()
+    // .subscribe((posts: any) => {
+    //   console.log(posts);
+    //   this.mensajes = posts;
+    // });
   }
 
 }
